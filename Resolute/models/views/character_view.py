07ea45a65c43506511e128743301d9e7f38768fd
Corvux89 <bryce.hadsell@gmail.com>
2025-01-17 19:23:01 -0600

    Refactor character type option logic to simplify conditions for new character creation

--- Resolute/models/views/character_view.py
+++ Resolute/models/views/character_view.py
@@ -169,9 +169,9 @@ class _NewCharacter(CharacterManage):
 
         new_character_type_options = []
         for type in ApplicationType:
-            if type.value == 'new':
-                if len(self.player.characters) == 0 or len(self.player.characters) < self.player.guild.max_characters and type.value == 'new':
-                    new_character_type_options.append(SelectOption(label=f"{type.value}", value=f"{type.name}", default=True if self.new_type == type else False))
+            if type == ApplicationType.new:
+                if len(self.player.characters) == 0 or len(self.player.characters) < self.player.guild.max_characters:
+                    new_character_type_options.append(SelectOption(label=f"{type.value}", value=f"{type.name}", default=True if self.new_type == type or len(self.player.characters) == 0 else False))
             elif len(self.player.characters) > 0:
                 new_character_type_options.append(SelectOption(label=f"{type.value}", value=f"{type.name}", default=True if self.new_type == type else False))
 
